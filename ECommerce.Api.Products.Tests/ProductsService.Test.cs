57666f55fad2099242952658b854cf5db7a73278
Aakash <aakashamatya63@gmail.com>
2020-07-13 00:36:53 -0400

    Implemented Unit test project to test different microservice using X Unit

---
+++ ECommerce.Api.Products.Tests/ProductsService.Test.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Profiles;
+using ECommerce.Api.Products.Providers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ECommerce.Api.Products.Tests
+{
+    public class ProductsService
+    {
+        [Fact]
+        public async void GetProductsReturnsAllProducts()
+        {
+            var options = new DbContextOptionsBuilder<ProductDbContext>().UseInMemoryDatabase(nameof(GetProductsReturnsAllProducts)).Options;
+            var dbContext = new ProductDbContext(options);
+            CreateProducts(dbContext);
+            var productsProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productsProfile));
+
+            var mapper = new Mapper(configuration);
+
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+            var product = await productsProvider.GetProductsAsync();
+            Assert.True(product.IsSuccess);
+            Assert.True(product.Products.Any());
+            Assert.Null(product.ErrorMessage);
+
+        }
+
+        [Fact]
+        public async void GetProductReturnsProductUsingInvalidId()
+        {
+            var options = new DbContextOptionsBuilder<ProductDbContext>().UseInMemoryDatabase(nameof(GetProductsReturnsAllProducts)).Options;
+            var dbContext = new ProductDbContext(options);
+            CreateProducts(dbContext);
+            var productsProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productsProfile));
+
+            var mapper = new Mapper(configuration);
+
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+            var product = await productsProvider.GetProductAsync(-1);
+            Assert.False(product.IsSuccess);
+            Assert.Null(product.Product);
+            Assert.NotNull(product.ErrorMessage);
+
+        }
+
+        [Fact]
+        public async void GetProductReturnsProductUsingValidId()
+        {
+            var options = new DbContextOptionsBuilder<ProductDbContext>().UseInMemoryDatabase(nameof(GetProductsReturnsAllProducts)).Options;
+            var dbContext = new ProductDbContext(options);
+            CreateProducts(dbContext);
+            var productsProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productsProfile));
+
+            var mapper = new Mapper(configuration);
+
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+            var product = await productsProvider.GetProductAsync(1);
+            Assert.True(product.IsSuccess);
+            Assert.True(product.Product.Id == 1);
+            Assert.Null(product.ErrorMessage);
+
+        }
+
+        private void CreateProducts(ProductDbContext dbContext)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                dbContext.Products.Add(new Product()
+                {
+                    Id = i,
+                    Name = Guid.NewGuid().ToString(),
+                    Inventory = i + 10,
+                    Price = (decimal)(i * 3.14)
+                });
+            }
+            dbContext.SaveChanges();
+        }
+    }
+}
